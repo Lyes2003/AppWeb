2c80ec0877ebce545a61020fc54c697719c8629f
Lyes2003 <chikhlyes55@gmail.com>
2026-05-14 00:50:19 -0400

    Ajout des register blueprints

--- MonSite/index.py
+++ MonSite/index.py
@@ -21,11 +21,12 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 from datetime import timedelta
 from urllib.parse import urlparse, urljoin
 from flask_wtf import CSRFProtect   # import: active CSRF via Flask-WTF
-from forms import AddCourseForm, DeleteCoursForm, AddChapterForm, RechercheForm, DeleteChapitreForm, DeleteDocumentForm
+from forms import AddCoursForm, AddChapterForm, RechercheForm, DeleteChapitreForm, DeleteDocumentForm
 from flask_wtf.csrf import CSRFError
 from werkzeug.utils import secure_filename
 from config import SECRET_KEY
 from auth.routes import auth
+from admin.gestioncours import admin
 from utils import get_db, User, is_safe_url, is_valid_email, is_valid_name, is_strong_password
 
 app = Flask(__name__, static_url_path="", static_folder="static")
@@ -53,6 +54,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'auth.login' # la redirection si l'utilisateur n'est pas connecté ou n'a pas les droits nécessaires
 
 app.register_blueprint(auth, url_prefix='/auth') # enregistrer le blueprint
+app.register_blueprint(admin, url_prefix='/admin') # enregistrer le blueprint admin
 
 
 # Flask-Login user loader
@@ -112,7 +114,7 @@ def admin():
     if not current_user.is_admin:
         abort(403)  # fait echouer la requête si l'utilisateur n'est pas admin
     db = get_db()
-    form = AddCourseForm()
+    form = AddCoursForm()
     cours = db.get_cours()
     chapitres = db.get_chapitres()
     documents = db.get_documents()
@@ -127,34 +129,6 @@ def admin():
 
     return render_template('admin.html', form=form, cours=cours, chapitres=chapitres, documents=documents)
 
-# route pour ajouter un cours ( seulement pour l'admin )
-# retourne à la page d'administration après l'ajout réussi d'un cours
-@app.route('/admin/Ajouter_Cours', methods=['GET', 'POST'])
-@login_required
-def add_cours():
-    if not current_user.is_admin:
-        abort(403)  # fait echouer la requête si l'utilisateur n'est pas admin
-    form = AddCourseForm()
-    if form.validate_on_submit():
-        nom_cours = form.nom_cours.data.strip()
-        description = form.description.data.strip()
-        db = get_db()
-        db.insert_cours(nom_cours, description)
-        flash(f'Le cours "{nom_cours}" a été ajouté avec succès.', 'success')
-        return redirect(url_for('admin'))
-    return render_template('insert_cours.html', form=form)
-
-# Route pour afficher les détails d'un cours
-# retourne la page de détails du cours spécifié par son ID
-@app.route('/cours/<int:id_cours>')
-@login_required
-def cours_detail(id_cours):
-    db = get_db()
-    cours = db.get_cours_par_id(id_cours) # obtenir les détails du cours
-    chapitres = db.get_chapitre_par_id_cours(id_cours) # obtenir les chapitres du cours
-    if not cours:
-        abort(404)
-    return render_template('cours_detail.html', cours=cours, chapitres=chapitres)
 
 # Route pour afficher les chapitres d'un cours
 # retourne la page des chapitres du cours spécifié par son ID
@@ -274,67 +248,6 @@ def supprimer_document():
             flash('Document non trouvé.', 'error')
     return redirect(url_for('admin'))
 
-# route pour supprimer un cours ( seulement pour l'admin MOI ! )
-# supprime aussi tous les chapitres, documents et fichiers associés (suppression récursive)
-# retourne à la page d'administration après la suppression réussie d'un cours
-@app.route('/admin/supprimer_cours', methods=['POST'])
-@login_required
-def supprimer_cours():
-    if not current_user.is_admin:
-        abort(403)
-    form = DeleteCoursForm()
-    if form.validate_on_submit():
-        id_cours = int(form.id_cours.data)
-    if not id_cours:
-        flash('Veuillez sélectionner un cours.', 'error')
-        return redirect(url_for('admin'))
-
-    db = get_db()
-    cours = db.get_cours_par_id(id_cours)
-
-    # si cours n'existe pas on affiche la page 404
-    if not cours:
-        abort(404) 
-
-    # Récupérer tous les chapitres du cours
-    chapitres = db.get_chapitre_par_id_cours(id_cours)
-
-    # Pour chaque chapitre on supprime les documents associés
-    for chapitre in chapitres:
-        documents = db.get_documents_par_chapitre(chapitre['id_chapitre'])
-        # Supprimer les fichiers PDF du serveur
-        for doc in documents:
-            file_path = os.path.join(app.root_path, doc['url_document'].lstrip('/')) # Chemin complet du fichier
-            if os.path.exists(file_path):
-                os.remove(file_path)  # Supprime le fichier du serveur
-            db.delete_document(doc['id_document'])  # Supprime le document de la BDD
-        # Supprimer le chapitre
-        db.delete_chapitre(chapitre['id_chapitre'])
-    
-    # Supprimer le cours
-    db.delete_cours(id_cours)
-    flash(f'Cours supprimé avec succès (avec tous ses chapitres et documents).', 'success')
-    return redirect(url_for('admin'))
-
-# route pour modifier un cours ( seulement pour l'admin )
-# retourne à la page d'administration après la modification réussie d'un cours
-@app.route('/admin/modifier_cours/<int:id_cours>', methods=['GET', 'POST'])
-@login_required
-def modifier_cours(id_cours):
-    if not current_user.is_admin:
-        abort(403)
-    db = get_db() 
-    cours = db.get_cours_par_id(id_cours) 
-    if not cours:
-        abort(404)
-    form = AddCourseForm(obj=cours)  # Pré-remplit le formulaire avec les données existantes
-    if form.validate_on_submit():
-        nom_cours = form.nom_cours.data.strip()
-        description = form.description.data.strip()
-        db.modify_cours(id_cours, nom_cours, description)
-        flash(f'Cours modifié avec succès.', 'success')
-        return redirect(url_for('admin'))
-    return render_template('insert_cours.html', form=form, edit=True, cours=cours) 
 
 @app.route('/admin/supprimer_chapitre', methods=['POST'])
 @login_required
