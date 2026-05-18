038a35dc3d50b1b43ab517444dc88248b80ceada
Lyes2003 <chikhlyes55@gmail.com>
2026-05-18 14:34:06 -0400

    refactor: reconstruction du code pour prendre en compte l'ajout du bleuprint de gestion documents

--- MonSite/index.py
+++ MonSite/index.py
@@ -27,6 +27,7 @@ from config import SECRET_KEY
 from auth.routes import auth
 from admin.gestioncours import managcours
 from admin.gestionchapitres import managchapters
+from admin.gestiondocuments import managdocuments
 from utils import get_db, User, is_safe_url, is_valid_email, is_valid_name, is_strong_password
 
 app = Flask(__name__, static_url_path="", static_folder="static")
@@ -56,6 +57,7 @@ login_manager.login_view = 'auth.login' # la redirection si l'utilisateur n'est 
 app.register_blueprint(auth, url_prefix='/auth') # enregistrer le blueprint 
 app.register_blueprint(managcours, url_prefix='/admin') # enregistrer le blueprint admin
 app.register_blueprint(managchapters, url_prefix='/admin') # enregistrer le blueprint admin
+app.register_blueprint(managdocuments, url_prefix='/admin') # enregistrer le blueprint admin
 
 
 # Flask-Login user loader
@@ -129,73 +131,6 @@ def admin():
         chapt['nbr_documents'] = db.count_documents_par_chapitre(chapt['id_chapitre']) # compte le nombre de documents par chapitre
 
     return render_template('admin.html', form=form, cours=cours, chapitres=chapitres, documents=documents)
-
-# Route pour afficher les fichiers PDF d'un chapitre d'un cours
-# retourne le fichier PDF du document spécifié par son ID
-@app.route('/document/<int:id_document>')
-@login_required
-def serve_document(id_document):
-    
-    # Récupérer le document depuis la base de données et vérifier les droits d'accès 
-    # avec le chapitre et le cours associés
-    
-    # initialize la base de données
-    db = get_db()
-    document = db.get_document(id_document)
-    
-    # Vérifier que le document existe
-    if not document:
-        abort(404)
-    
-    # Récupérer le chapitre pour vérifier les droits
-    chapitre = db.get_chapitre_par_id(document['id_chapitre'])
-    
-    # Vérifier que le chapitre existe
-    if not chapitre:
-        abort(404)
-    
-    
-    # Construire le chemin du fichier
-    # Contient le chemin du fichier stocké avec son nom
-    file_path = os.path.join(app.root_path, document['url_document'])
-    
-    # Vérifier que le fichier existe et qu'on n'essaie pas d'accéder hors du dossier uploads/
-    real_path = os.path.realpath(file_path)  # Résoudre les ../ et symlinks
-    upload_folder_real = os.path.realpath(app.config['UPLOAD_FOLDER']) # Chemin réel du dossier uploads
-    
-    # verifie que le chemain commance bien par le dossier uploads et que le fichier existe
-    if not real_path.startswith(upload_folder_real) or not os.path.exists(real_path):
-        abort(403)  # Accès refusé
-    
-    # Servir le fichier
-    return send_file(
-        real_path,
-        as_attachment=False,  # Afficher dans le navigateur 
-        mimetype='application/pdf'
-    )
-
-@app.route('/admin/supprimer_document', methods=['POST'])
-@login_required
-def supprimer_document():
-    if not current_user.is_admin:
-        abort(403)
-    form = DeleteDocumentForm()
-    if form.validate_on_submit():
-        id_document = int(form.id_document.data)
-        # Récupérer le document depuis la base de données
-        db = get_db()
-        document = db.get_document(id_document)
-        if document:
-            # Supprimer le fichier du serveur
-            file_path = os.path.join(app.root_path, document['url_document'].lstrip('/'))
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            # Supprimer le document de la base de données
-            db.delete_document(id_document)
-            flash('Document supprimé avec succès.', 'success')
-        else:
-            flash('Document non trouvé.', 'error')
-    return redirect(url_for('admin'))
 
 @app.route('/recherche', methods=['POST'])  
 @login_required
